f9dc7744913bfebaafddaf08374a3d5a69c18d46
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-07 21:08:34 +0530

    Max minus Min
    Chef is given 3 integers A, B and C such that A<B<C.
    
    Chef needs to find the value of max(A, B, C) - min(A, B, C).
    
    Here max(A, B, C) denotes the maximum value among A, B, C while min(A, B, C) denotes the minimum value among A, B, C.

---
+++ MAXDIFFMIN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+	while (T--)
+	{
+	    int A;int B;int C;
+	    cin >> A >>B >>C;
+	    int diff=C-A;
+	    cout <<diff<<endl;
+	}
+	return 0;
+}
