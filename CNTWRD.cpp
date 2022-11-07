f66cbc606d162db5680ee863ba85eacde7a13e8f
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 01:07:54 +0530

    Counting Words
    ##PROBLEM
    Harsh was recently gifted a book consisting of N pages. Each page contains exactly M words printed on it. As he was bored, he decided to count the number of words in the book.
    
    Help Harsh find the total number of words in the book.

---
+++ CNTWRD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while (t--){
+	    int N;int M;
+	    cin >> N >> M;
+	    int T;
+	    T=N*M;
+	    cout <<T <<endl;
+	}
+	return 0;
+}
