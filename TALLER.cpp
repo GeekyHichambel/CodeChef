868d713b5a06ebf280ac5b05a512f3d877292d93
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 00:42:49 +0530

    Who is taller!
    Alice and Bob were having an argument about which of them is taller than the other. Charlie got irritated by the argument, and decided to settle the matter once and for all.
    
    Charlie measured the heights of Alice and Bob, and got to know that Alice's height is X centimeters and Bob's height is Y centimeters. Help Charlie decide who is taller.
    
    It is guaranteed that X !=Y.

---
+++ TALLER.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while (t--){
+	    int X;int Y;
+	    cin >> X >>Y;
+	    if (X>Y){
+	        cout << "A"<<endl;
+	    }
+	    else {
+	        cout << "B"<<endl;
+	    }
+	}
+	return 0;
+}
