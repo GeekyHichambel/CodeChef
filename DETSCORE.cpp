5c36660b4cbd2c022bdb2973d6ea245923d009a4
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-07 23:44:32 +0530

    Determine the Score
    #Problem
    Chef appeared for a placement test.
    
    There is a problem worth X points. Chef finds out that the problem has exactly 10 test cases. It is known that each test case is worth the same number of points.
    
    Chef passes N test cases among them. Determine the score Chef will get.

---
+++ DETSCORE.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--){
+	    int X; int N;
+	    cin >> X >> N;
+	    int t;
+	    X=X/10;
+	    N=N*X;
+	    cout << N << endl; 
+	}
+	return 0;
+}
