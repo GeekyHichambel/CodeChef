81dd5a718b36f49c0244e06102ca348ca875b25a
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 00:12:07 +0530

    Reach the Target
    ##PROBLEM
    There is a cricket match going on between two teams A and B.
    
    Team B is batting second and got a target of X runs. Currently, team B has scored Y runs. Determine how many more runs Team B should score to win the match.
    
    Note: The target score in cricket matches is one more than the number of runs scored by the team that batted first.

---
+++ REACHTHETARGET.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--){
+	    int X;int Y;
+	    cin >> X>>Y;
+	    X=X-Y;
+	    cout << X<<endl;
+	}
+	return 0;
+}
