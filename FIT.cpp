267ede0b68f72d2fc10c925a722e2a04f3d4cd59
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-07 23:22:25 +0530

    Fitness
    Chef wants to become fit for which he decided to walk to the office and return home by walking. It is known that Chef's office is X km away from his home.
    
    If his office is open on 5 days in a week, find the number of kilometers Chef travels through office trips in a week.

---
+++ FIT.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--)
+	{
+	    int X;
+	    cin >> X;
+	    int w;
+	    w=10*X;
+	    cout << w <<endl;
+	}
+	return 0;
+}
