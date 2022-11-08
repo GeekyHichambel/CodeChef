07e9d2d4f4dde4086b0d97911c6a14e91d24016b
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 15:27:23 +0530

    Apples and Oranges
    ##Problem
    Bob has X rupees and goes to a market. The cost of apples is Rs. A per kg and the cost of oranges is Rs. B per kg.
    
    Determine whether he can buy at least 1 kg each of apples and oranges.

---
+++ APPLORNG.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int X;int A,B;
+	cin >>X;
+	cin >> A>>B;
+	if (A+B<=X)
+	{
+	    cout <<"YES"<<endl;
+	}
+	else{
+	    cout <<"NO"<<endl;
+	}
+	return 0;
+}
