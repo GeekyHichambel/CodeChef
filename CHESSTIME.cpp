7c35a69ab984768b9027773dd5bb34170a496ec5
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 14:36:54 +0530

    Chess Time
    ##PROBLEM
    Chef has recently started playing chess, and wants to play as many games as possible.
    
    He calculated that playing one game of chess takes at least 20 minutes of his time.
    
    Chef has N hours of free time. What is the maximum number of complete chess games he can play in that time?

---
+++ CHESSTIME.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--){
+	    int N;
+	    cin >>N;
+	    N=N*60;
+	    int max;
+	    max=N/20;
+	    cout <<max<<endl;
+	}
+	return 0;
+}
