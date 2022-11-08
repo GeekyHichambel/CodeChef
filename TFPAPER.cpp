14bddf260fcc9231bd7e6b8817ce66cd60531484
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 19:43:50 +0530

    True and False Paper
    ##PROBLEM
    Alice wrote an exam containing N true or false questions (i.e. questions whose answer is either true or false). Each question is worth 1 mark and there is no negative marking in the examination. Alice scored K marks out of N.
    
    Bob wrote the same exam but he marked each and every question as the opposite of what Alice did, i.e, for whichever questions Alice marked true, Bob marked false and for whichever questions Alice marked false, Bob marked true.
    
    Determine the score of Bob.

---
+++ TFPAPER.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--){
+	    int N;int K;
+	    cin >>N>>K;
+	    N=N-K;
+	    cout << N <<endl;
+	}
+	return 0;
+}
