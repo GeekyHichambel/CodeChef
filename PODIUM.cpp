0c4fae8e45e143f9de355269052438ddb35e8135
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 14:42:30 +0530

    Podium Finish
    ##PROBLEM
    Chef got his dream seat in F1 and secured a 3rd place in his debut race. He now wants to know the time gap between him and the winner of the race.
    
    You are his chief engineer and you only know the time gap between Chef and the runner up of the race, given as A seconds, and the time gap between the runner up and the winner of the race, given as B seconds.
    
    Please calculate and inform Chef about the time gap between him and the winner of the race.

---
+++ PODIUM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--)
+	{
+	    int A;int B;
+	    cin >>A>>B;
+	    A=A+B;
+	    cout <<A<<endl;
+	}
+	return 0;
+}
