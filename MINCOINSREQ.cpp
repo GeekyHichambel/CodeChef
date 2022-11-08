0a287e34e839171ab928df2743079b7ff66bc417
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 19:29:14 +0530

    Minimum Coins
    ##PROBLEM
    There are only 2 type of denominations in Chefland:
    
    Coins worth 1 rupee each
    Notes worth 10 rupees each
    Chef wants to pay his friend exactly X rupees. What is the minimum number of coins Chef needs to pay exactly X rupees?

---
+++ MINCOINSREQ.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int t;
+	  cin >>t;
+	  while(t--){
+	      int X;
+	      cin >>X;
+	      X=X%10;
+	      cout << X <<endl;
+	  }
+	return 0;
+}
