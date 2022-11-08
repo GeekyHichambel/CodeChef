43d24bda03ce9de5f2ee473ad3cc1ee015171c7d
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 12:47:21 +0530

    Number of Credits
    ##PROBLEM
    In the current semester, you have taken X RTP courses, Y Audit courses and Z Non-RTP courses.
    
    The credit distribution for the courses are:
    
    4 credits for clearing each RTP course.
    2 credits for clearing each Audit course.
    No credits for clearing a Non-RTP course.
    Assuming that you cleared all your courses, report the number of credits you obtain this semester.

---
+++ CREDS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin >>x>>y>>z;
+	    x=x*4;
+	    y=y*2;
+	    cout << x+y <<endl;
+	}
+	return 0;
+}
