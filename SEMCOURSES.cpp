1575caca4b874286c483ac2d4613236769fd72c9
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-08 15:08:06 +0530

    Chef and Chapters
    ##PROBLEM
    This semester, Chef took X courses. Each course has Y units and each unit has Z chapters in it.
    
    Find the total number of chapters Chef has to study this semester.

---
+++ SEMCOURSES.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >>t;
+	while (t--){
+	    int X;int Y; int Z;
+	    cin >>X>>Y>>Z;
+	    X=X*(Y*Z);
+	    cout << X<<endl;
+	}
+	return 0;
+}
