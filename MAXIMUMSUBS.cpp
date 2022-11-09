11c153a4d60666de7082dc54ef33a696aabe2c21
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-10 01:29:52 +0530

    Maximum Submissions
    A participant can make 1 submission every 30 seconds. If a contest lasts for X minutes, what is the maximum number of submissions that the participant can make during it?
    
    It is also given that the participant cannot make any submission in the last 5 seconds of the contest.

---
+++ MAXIMUMSUBS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin >>t;
+	 while (t--){
+	     int x;
+	     cin >>x;
+	     x=x*2;
+	     cout << x<<endl;
+	 }
+	return 0;
+}
