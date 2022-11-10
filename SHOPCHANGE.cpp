f3e9a4d332fbdcc1e72a385e5c31d1476dfb376c
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-10 21:44:47 +0530

    Shopping Change
    Chef went shopping and bought items worth X rupees (1≤X≤100). Unfortunately, Chef only has a single 100 rupees note.
    
    Since Chef is weak at maths, can you help Chef in calculating what money he should get back after paying 100 rupees for those items?

---
+++ SHOPCHANGE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin >> t;
+	 while(t--){
+	     int X;
+	     cin >> X;
+	     X=100-X;
+	     cout << X <<endl;
+	 }
+	return 0;
+}
