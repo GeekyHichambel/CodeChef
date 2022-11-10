a833aa2b122c5dffa48c30cde211447076fb65c0
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-11 01:37:19 +0530

    Sugarcane Juice Business
    While Alice was drinking sugarcane juice, she started wondering about the following facts:
    
    The juicer sells each glass of sugarcane juice for 50 coins.
    He spends 20% of his total income on buying sugarcane.
    He spends 20% of his total income on buying salt and mint leaves.
    He spends 30% of his total income on shop rent.
    Alice wonders, what is the juicer's profit (in coins) when he sells N glasses of sugarcane juice?

---
+++ SUGARCANE.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     int t;
+     std::cin >> t;
+     while(t--){
+         int N;
+         cin >>N;
+         N=N*15;
+         cout << N <<endl;
+     }
+	return 0;
+}
