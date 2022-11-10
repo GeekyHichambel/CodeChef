83d683c17bf94fc9aac32206a7dbbbfaeefe816e
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-11 01:26:08 +0530

    Count the Notebooks
    You know that 1 kg of pulp can be used to make 1000 pages and 1 notebook consists of 100 pages.
    
    Suppose a notebook factory receives N kg of pulp, how many notebooks can be made from that?

---
+++ NOTEBOOK.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin >> t;
+	 while(t--){
+	     int N;
+	     cin >> N;
+	     N=N*10;
+	     cout << N<<endl;
+	 }
+	return 0;
+}
