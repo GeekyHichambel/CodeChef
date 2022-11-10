93e0ebf43345e5f9810a043b65aae0aefb5211a7
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-10 22:01:02 +0530

    Chef in his Office
    Recently Chef joined a new company. In this company, the employees have to work for X hours each day from Monday to Thursday. Also, in this company, Friday is called Chill Day — employees only have to work for Y hours (Y<X) on Friday. Saturdays and Sundays are holidays.
    
    Determine the total number of working hours in one week.

---
+++ OFFICE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t;
+	 cin >> t;
+	 while(t--){
+	     int X;int Y;
+	     cin >>X>>Y;
+	     X=X*4;
+	     Y=X+Y;
+	     std::cout << Y << std::endl;
+	 }
+	return 0;
+}
