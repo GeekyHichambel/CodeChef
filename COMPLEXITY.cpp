a08cb199aecc923c47f9d24e1deeeccf9cdc94a2
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-14 01:41:07 +0530

    Time Complexity
    A sorting algorithm A is said to have more time complexity than a sorting algorithm B if it uses more number of comparisons for sorting the same array than algorithm B.
    
    Given that algorithm A uses X comparisons to sort an array and algorithm B uses Y comparisons to sort the same array, find whether algorithm A has more time complexity.

---
+++ COMPLEXITY.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	 int t;
+	 std::cin >> t;
+	 while (t--){
+	     int X;int Y;
+	     cin >>X>>Y;
+	     if (X>Y){
+	         cout << "YES"<<endl;
+	     }
+	     else{
+	         cout << "NO" <<endl;
+	     }
+	 }
+	return 0;
+}
