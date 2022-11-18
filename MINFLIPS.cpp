dd507fa7d7c260a664c92a9f30ac2bc90bdfb0bc
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-18 19:42:29 +0530

    Minimum Flips
    Chef has an array A of length N consisting of 1 and −1 only.
    
    In one operation, Chef can choose any index i (1≤i≤N) and multiply the element A_i by -1.
    Find the minimum number of operations required to make the sum of the array equal to 0. Output -1 if the sum of the array cannot be made 0.

--- MINFLIPS.cpp
+++ MINFLIPS.cpp
@@ -13,22 +13,20 @@ int main() {
 	     for (int i=0;i<n;i++)
 	     {
 	         cin >> arr[i];
+	         if (arr[i]==1){
+	             count++;
+	         }
+	         else{
+	             s++;
+	         }
 	     }
 	      if (n%2!=0){
 	         cout << "-1" << endl;
 	     }
 	     else{
-	     for (int i=0;i<n;i++){
-	         if (arr[i]==1){
-	             count++;
-	         }
-	         else{
-	           s++;      
-	         }
+	        int fin = abs(count-s)/2;
+	        cout << fin <<endl;
 	      }
-	      int fin= abs(count-s)/2;
-	      cout << fin <<endl;
-	     }
 	 }
 	return 0;
 }
