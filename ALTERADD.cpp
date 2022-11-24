7326132fd93311bd293a280e8414c30a51965c57
Parth Vij <109455495+GeekyHichambel@users.noreply.github.com>
2022-11-24 21:57:05 +0530

    Alternate Additions
    Chef has 2 numbers A and B (A<B).
    
    Chef will perform some operations on A.
    
    In the ith operation:
    
    Chef will add 1 to A if i is odd.
    Chef will add 2 to A if i is even.
    Chef can stop at any instant. Can Chef make A equal to B?

---
+++ ALTERADD.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int t;
+	  cin >> t;
+	  while(t--){
+	      int a,b;
+	      cin >> a >>b;
+	      if ((b-a)%3==0||(b-a)%3==1){
+	          cout << "YES" <<endl;
+	      }
+	      else{
+	          cout << "NO" <<endl;
+	      }
+	  }
+	return 0;
+}
